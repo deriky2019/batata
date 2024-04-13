d565342b033e87e64f38b71ee8df6c54614c0fc3
deriky2019 <derikyjoelson@gmail.com>
2024-04-13 11:02:43 -0300

    de

---
+++ escola/matematica/1 -ano.js
@@ -0,0 +1,14 @@
+let Q1 = Number (document.getElementById("Q1"));value
+let res = document.getElementById("resultadoQ1");value
+
+
+
+function calcular() {
+  if (Q1 == 17) {
+    res.innerHTML = "acertou"
+    
+  } else {res.innerHTML = "errou  resultado era 17"
+    
+  }
+    
+}
