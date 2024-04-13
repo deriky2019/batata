c579c31fb64a3d78270305f3eecf197c167140fd
deriky2019 <derikyjoelson@gmail.com>
2024-04-13 11:08:36 -0300

    d

--- escola/matematica/1 -ano.js
+++ escola/matematica/1 -ano.js
@@ -1,7 +1,9 @@
 let Q1 = Number (document.getElementById("Q1"));value
+let Q2
+let Q3
+let Q4
+let Q5
 let res = document.getElementById("resultadoQ1");value
-
-
 
 function calcular() {
   if (Q1 == 17) {
