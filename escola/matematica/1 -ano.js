0bb9309e817725a0a080b8891680a3c84c872f05
deriky2019 <derikyjoelson@gmail.com>
2024-04-13 16:53:36 -0300

    de

--- escola/matematica/1 -ano.js
+++ escola/matematica/1 -ano.js
@@ -1,16 +1,23 @@
-let Q1 = Number (document.getElementById("Q1"));value
-let Q2
-let Q3
-let Q4
-let Q5
-let res = document.getElementById("resultadoQ1");value
 
 function calcular() {
-  if (Q1 == 17) {
-    res.innerHTML = "acertou"
+    let Q1 = Number(document.getElementById("Q1").value);
+    let Q2 = Number(document.getElementById("Q2").value);
+    let Q3 = Number(document.getElementById("Q3").value);
+    let Q4 = Number(document.getElementById("Q4").value);
+    let Q5 = Number(document.getElementById("Q5").value);
     
-  } else {res.innerHTML = "errou  resultado era 17"
     
+    let res = document.getElementById("resultadoQ1");
+    let res2 = document.getElementById("resultadoQ2");
+    let res3 = document.getElementById("resultadoQ3");
+    let res4 = document.getElementById("resultadoQ4");
+    let res5 = document.getElementById("resultadoQ5");
+   
+  
+    if () {
+      res.innerHTML = "acertou";
+    } else {
+      res.innerHTML = "errou. O resultado era 17";
+    }
   }
-    
-}
+  
