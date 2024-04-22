bf523079d65ad175aec25949e503cf912c9e2ac0
deriky2019 <derikyjoelson@gmail.com>
2024-04-22 13:01:22 -0300

    de

--- filmes-main/tela-inicial.js
+++ filmes-main/tela-inicial.js
@@ -1,21 +1,25 @@
 let  muda = document.getElementById("mudar")
 let  claro = document.getElementById("branco")
 let  escuro = document.getElementById("preto")
-let  menu = document.getElementById("temas")
+let  temas = document.getElementById("temas")
 let  atores = document.getElementById("atores")
+let  hyper = document.getElementById("hyper")
 function preto() {
     muda.style.backgroundColor = "black"
     escuro.style.display = "none"
     claro.style.display = "block"
-    menu.style.color = "blue"
-    atores.style.color = "blue"
+    temas.style.color = "white"
+    atores.style.color = "white"
+    hyper.style.backgroundColor = "black"
 
 } 
 function branco() {
+    muda.style.backgroundColor = "white"
     claro.style.display = "none"
     escuro.style.display = "block"
-    muda.style.backgroundColor = "white"
-    menu.style.color = "black"
+    temas.style.color = "black"
+    atores.style.color = "black"
+    hyper.style.backgroundColor = "white"
 
     
 }
