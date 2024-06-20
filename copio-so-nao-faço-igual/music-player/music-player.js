38612c370963fe1b029b681a90a3809015236a28
deriky2019 <derikyjoelson@gmail.com>
2024-06-19 22:07:07 -0300

    de

---
+++ copio-so-nao-faço-igual/music-player/music-player.js
@@ -0,0 +1,14 @@
+function pausar() {
+    let play  = document.getElementById("play")
+    let pause = document.getElementById("pause")
+    pause.style.display = "block"
+    play.style.display = "none"
+
+}
+
+function despausar() {
+    let play  = document.getElementById("play")
+    let pause = document.getElementById("pause")
+    pause.style.display = "none"
+    play.style.display = "block"
+}
