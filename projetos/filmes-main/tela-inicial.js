861a5d378ee5228fc46f802c518e4dac94a74dd9
deriky2019 <derikyjoelson@gmail.com>
2024-08-06 19:30:54 -0300

    de

--- projetos/filmes-main/tela-inicial.js
+++ projetos/filmes-main/tela-inicial.js
@@ -24,3 +24,19 @@ function branco() {
 
     
 }
+
+function verificarLarguraJanela() {
+    let  = document.getElementById("input");
+    if (window.innerWidth < 690) {
+        input.style.display = "none";
+    } else {
+        input.style.display = "block"; // ou "inline", dependendo do estilo desejado
+    }
+}
+
+// Verificar a largura da janela quando a página carregar
+window.onload = verificarLarguraJanela;
+
+// Verificar a largura da janela sempre que ela for redimensionada
+window.onresize = verificarLarguraJanela;
+
