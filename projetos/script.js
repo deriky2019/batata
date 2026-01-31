0bbd5b85a22d3b62ec533b96abbb1c7bed8f0a47
deriky2019 <153954387+deriky2019@users.noreply.github.com>
2026-01-31 10:51:13 -0300

    Criar script.js

---
+++ projetos/script.js
@@ -0,0 +1,15 @@
+
+let loa = document.getElementById("pag2");
+let lob = document.getElementById("pag1");
+function lojaz () {
+loa.style.display = "block"
+lob.style.display = "none"
+
+}
+
+
+function sobrez () {
+loa.style.display = "none"
+lob.style.display = "block"
+
+}
