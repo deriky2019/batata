82a68d23301854b0c6dbd36734500665f23671d1
deriky2019 <153954387+deriky2019@users.noreply.github.com>
2026-03-23 21:39:58 -0300

    Criar JS.js

---
+++ projetos/Cart/Simon/JS.js
@@ -0,0 +1,48 @@
+
+let des1 = document.getElementById("des1")
+let des2 = document.getElementById("des2")
+let des9 = document.getElementById("des9")
+let bu1 = document.getElementById("bu1")
+let bu2 = document.getElementById("bu2")
+let bu3 = document.getElementById("bu3")
+let bu4 = document.getElementById("bu4")
+
+
+let link = document.getElementById("link")
+
+
+
+
+
+function linhaA() {
+    des1.style.display = "block"
+bu1.style.display = "none"
+bu2.style.display = "none"
+link.style.display = "block"
+}
+
+ 
+function linhaB() {
+    des2.style.display = "block"
+bu1.style.display = "none"
+bu2.style.display = "none"
+bu3.style.display = "inline"
+bu4.style.display = "inline"
+
+    
+    
+}
+
+function linhaB2() {
+    
+bu3.style.display = "none";
+des9.style.display = "block";
+}
+
+
+function linhaB3() {
+        
+bu3.style.display = "none"
+bu4.style.display = "none"
+
+}
