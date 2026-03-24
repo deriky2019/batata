be879a045f9599b2a9fe7633bea99c3a27a4c6b6
deriky2019 <153954387+deriky2019@users.noreply.github.com>
2026-03-24 00:36:09 -0300

    Criar js2.js

---
+++ projetos/Cart/Simon/js2.js
@@ -0,0 +1,71 @@
+
+let des1 = document.getElementById("des1")
+let des1_1 = document.getElementById("des1_1")
+let des1_2= document.getElementById("des1_2")
+let des2 = document.getElementById("des2")
+let des3 = document.getElementById("des3")
+let bu1 = document.getElementById("bu1")
+let bu1_1 = document.getElementById("bu1_1")
+let bu1_2 = document.getElementById("bu1_2")
+let bu3 = document.getElementById("bu3")
+let bu4 = document.getElementById("bu4")
+let bu2 = document.getElementById("bu2")
+let des4 = document.getElementById("des4")
+
+
+
+
+function linhaA() {
+    des1.style.display = "block"
+bu1.style.display = "none"
+bu2.style.display = "none"
+bu1_1.style.display = "inline"
+bu1_2.style.display = "inline"
+
+}
+
+
+
+
+function linhaA2() {
+    des1_1.style.display = "block"
+bu1_1.style.display = "none"
+bu1_2.style.display = "none"
+}
+
+
+
+function linhaA3() {
+    des1_2.style.display = "block"
+bu1_1.style.display = "none"
+bu1_2.style.display = "none"
+}
+
+
+ 
+function linhaB() {
+    des2.style.display = "block"
+bu1.style.display = "none"
+bu2.style.display = "none"
+bu3.style.display = "inline"
+bu4.style.display = "inline"
+
+    
+    
+}
+
+function linhaB2() {
+    
+bu3.style.display = "none"
+bu4.style.display = "none"
+des3.style.display = "block"
+}
+
+
+function linhaB3() {
+        
+bu3.style.display = "none"
+bu4.style.display = "none"
+des4.style.display = "block"
+
+}
